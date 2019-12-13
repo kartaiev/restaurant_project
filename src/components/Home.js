5a9507bcc9abe72c7cc989e5c411777123567f34
Volodymyr Kartaiev <kartayev@gmail.com>
2019-12-13 17:10:05 +0200

    video-background

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import bcVideo from '../assets/video/bcvideo.mp4';
 export const Home = () => {
   return (
     <div className="video-wrapper">
-      <video id="background-video" loop autoPlay>
+      <video id="background-video" preload="true" loop autoPlay>
         <source src={bcVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
