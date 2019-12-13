0a7237759bc2e5f13dba129cf297928a1f659413
Volodymyr Kartaiev <kartayev@gmail.com>
2019-12-13 23:31:33 +0200

    video-player

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './Home.scss';
 import bcVideo from '../assets/video/bcvideo.mp4';
 import title from '../assets/images/HS_Logo.png';
+import ReactPlayer from 'react-player';
 
 export const Home = () => {
   return (
     <div className="video-wrapper">
-      <video id="background-video" preload="true" loop autoPlay>
-        <source src={bcVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <div className="player-wrapper">
+        <ReactPlayer
+          playing
+          loop
+          muted
+          url={bcVideo}
+          className="react-player"
+          width="100%"
+          height="100%"
+        />
+      </div>
       <img className="title" src={title} alt="title" />
     </div>
   );
