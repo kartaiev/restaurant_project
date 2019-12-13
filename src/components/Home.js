a561eee0d283400cc2ea4c39c45713728164079d
Volodymyr Kartaiev <kartayev@gmail.com>
2019-12-14 00:42:19 +0200

    video-player

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,18 @@ import React from 'react';
 import './Home.scss';
 import bcVideo from '../assets/video/bcvideo.mp4';
 import title from '../assets/images/HS_Logo.png';
-import ReactPlayer from 'react-player';
 
 export const Home = () => {
   return (
     <div className="video-wrapper">
-      <div className="player-wrapper">
-        <ReactPlayer
-          playing
-          loop
-          muted
-          url={bcVideo}
-          className="react-player"
-          width="100%"
-          height="100%"
-        />
-      </div>
+      <video
+        autoPlay
+        loop
+        muted
+        preload="true"
+        src={bcVideo}
+        className="video-player"
+      />
       <img className="title" src={title} alt="title" />
     </div>
   );
