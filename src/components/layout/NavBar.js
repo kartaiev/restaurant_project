b4e51334254b87237e83bf23dab68447b51335e8
Volodymyr Kartaiev <kartayev@gmail.com>
2019-12-14 22:22:20 +0200

    nav-bar elements animation

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -8,22 +8,22 @@ export const NavBar = ({ classEl }) => {
       <div className="nav-bar__wrap">
         <ul>
           <li className={`li-style  ${classEl}`}>
-            <NavLink className="link" to="/about">
+            <NavLink className={`link ${classEl}`} to="/about">
               About
             </NavLink>
           </li>
           <li className={`li-style  ${classEl}`}>
-            <NavLink className="link" to="/gallery">
+            <NavLink className={`link ${classEl}`} to="/gallery">
               Gallery
             </NavLink>
           </li>
           <li className={`li-style  ${classEl}`}>
-            <NavLink className="link" to="/menu">
+            <NavLink className={`link ${classEl}`} to="/menu">
               Menu
             </NavLink>
           </li>
           <li className={`li-style  ${classEl}`}>
-            <NavLink className="link" to="/contact">
+            <NavLink className={`link ${classEl}`} to="/contact">
               Contact
             </NavLink>
           </li>
