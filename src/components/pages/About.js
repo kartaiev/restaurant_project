02f35eb1b3b29d2d8a62d31eea4ecf37130efada
Volodymyr Kartaiev <kartayev@gmail.com>
2019-12-15 23:44:12 +0200

    about-img

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './About.scss';
 import standingSteak from '../../assets/images/steak-1.jpg';
+import border from '../../assets/images/border.png';
 
 export const About = () => {
   return (
     <article className="about">
       <div className="about__img-wrap">
         <img className="about__img" src={standingSteak} alt="standing steak" />
+        <img className="border__img" src={border} alt="border" />
       </div>
       <section className="about__content">
-        <h1 className="about__content-title">About</h1>
+        <h2 className="about__content-title">About</h2>
         <p className="about__content-text">
           Overlooking Botany Bay on the Grand Parade of Brighton Le Sands sits a
           cosy Greek restaurant. For over five years, Meet The Greek has been
