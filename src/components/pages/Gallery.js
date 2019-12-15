19e0d15e542d6afce2a49358c6dcad4abd011b90
Volodymyr Kartaiev <kartayev@gmail.com>
2019-12-15 15:43:21 +0200

    pages

---
+++ src/components/pages/Gallery.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Gallery.scss';
+
+export const Gallery = () => {
+  return (
+    <div>
+      <h1>Gallery</h1>
+    </div>
+  );
+};
---
+++ src/components/pages/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Menu.scss';
+
+export const Menu = () => {
+  return (
+    <div>
+      <h1>Menu</h1>
+    </div>
+  );
+};
