d4c0120c4b10ef660fb98d31166fcd387bfac0ef
Volodymyr Kartaiev <kartayev@gmail.com>
2019-12-18 23:39:02 +0200

    shrink photo

--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Logo.scss';
-import steakhouse from '../../assets/images/charc-2.png';
+import steakhouse from '../../assets/images/steakhouse.png';
 
 
 export const Logo = () => {
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './About.scss';
-import standingSteak from '../../assets/images/steak-1.jpg';
+import standingSteak from '../../assets/images/steak-3.png';
 
 export const About = () => {
   return (
