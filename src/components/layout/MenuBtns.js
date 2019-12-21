049f06c9abbd88036e8024702e41709bb42611ff
Volodymyr Kartaiev <kartayev@gmail.com>
2019-12-21 17:54:25 +0200

    menu-buttons style toggle

--- src/components/layout/MenuBtns.js
+++ src/components/layout/MenuBtns.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './MenuBtns.scss';
 
 export const MenuBtns = () => {
+  const [btnActiveState, setBtnActiveState] = useState({
+    isActive: true,
+    activeStyle: {
+      border: '#9d231f',
+      color: '#ffb915'
+    }
+  });
+
+  const { isActive, activeStyle } = btnActiveState;
+
+  const addActiveClass = () => {
+    isActive
+      ? setBtnActiveState({ isActive: false })
+      : setBtnActiveState({ isActive: true });
+  };
+
   return (
     <div className="btns">
       <div className="btns__wrap">
         <Link className="btns__link" to="/menu">
-          <button className={`btns__link-btn`}>Food</button>
+          <button
+            style={isActive ? { activeStyle } : null}
+            className={`btns__link-btn`}
+            onClick={addActiveClass}
+          >
+            Food
+          </button>
         </Link>
 
         <Link className="btns__link" to="/menu/drinks">
-          <button className={`btns__link-btn`}>Drinks</button>
+          <button
+            style={isActive ? null : { activeStyle }}
+            className={`btns__link-btn`}
+            onClick={addActiveClass}
+          >
+            Drinks
+          </button>
         </Link>
       </div>
     </div>
