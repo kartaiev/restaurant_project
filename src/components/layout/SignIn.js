a92a08b93f3469d6e8e862303e5467fdafbbfa8f
Volodymyr Kartaiev <kartayev@gmail.com>
2020-01-01 18:29:55 +0200

    sign in

--- src/components/layout/SignIn.js
+++ src/components/layout/SignIn.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import { Button, SignInForm } from '../../elements';
+import { Button } from '../../elements';
+import styled from 'styled-components';
+import {
+  absolute,
+  background,
+  easeOut,
+  grey,
+  red,
+  yellow
+} from '../../utilities';
 
 const SignIn = () => {
   return (
-    <SignInForm modifiers="signIn">
+    <Sign>
       <div>
-        <input name="email" type="email" required />
-        <label htmlFor="email">
-          <span>Email</span>
-        </label>
+        <input name="email" type="email" required placeholder="email" />
+        <label htmlFor="email" />
       </div>
       <div>
-        <input name="password" type="password" required />
-        <label htmlFor="password">
-          <span>Password</span>
-        </label>
+        <input
+          name="password"
+          type="password"
+          required
+          placeholder="password"
+        />
+        <label htmlFor="password" />
       </div>
       <Button modifiers="big">
         <span>Sign In</span>
       </Button>
-    </SignInForm>
+    </Sign>
   );
 };
 
 export default SignIn;
+
+const Sign = styled.form`
+  width: 100%;
+  height: 40vh;
+  margin-top: 20vh;
+
+  div {
+    width: 100%;
+    height: 30%;
+    position: relative;
+
+    input {
+      width: 100%;
+      height: 100%;
+      background: ${background};
+      border: none;
+      color: ${grey};
+      padding-top: 25px;
+      ${easeOut};
+
+      &:focus + label::after,
+      &:valid + label::after {
+        background-position: left center;
+      }
+
+      &::placeholder {
+        color: ${grey};
+      }
+    }
+    label {
+      width: 100%;
+      height: 100%;
+      ${absolute({ yProp: 'bottom', y: '0' })};
+      pointer-events: none;
+
+      &::after {
+        content: '';
+        height: 1px;
+        width: 100%;
+        ${absolute({ yProp: 'bottom', y: '0' })};
+        background-image: linear-gradient(to right, ${red} 50%, ${yellow} 50%);
+        background-size: 200% 100%;
+        background-position: right center;
+        ${easeOut};
+      }
+    }
+  }
+
+  button {
+    margin-top: 5vh;
+  }
+`;
