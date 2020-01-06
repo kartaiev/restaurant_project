14f0e65884d1cdbd9ce1ac2b24c78813aec92657
Volodymyr Kartaiev <kartayev@gmail.com>
2020-01-06 14:38:32 +0200

    gallery

--- src/assets/images/gallery/index.js
+++ src/assets/images/gallery/index.js
@@ -3,7 +3,6 @@ import loft2 from './2.jpg';
 import loft3 from './3.jpg';
 import loft4 from './4.jpg';
 import loft5 from './5.jpg';
-import loft1L from './1L.jpeg';
 import loft2L from './2L.jpeg';
 import loft3L from './3L.jpeg';
 import loft4L from './4L.jpg';
@@ -14,7 +13,6 @@ export const pics1 = [
   loft3,
   loft4,
   loft5,
-  loft1L,
   loft2L,
   loft3L,
   loft4L,
@@ -26,7 +24,6 @@ export { loft2 };
 export { loft3 };
 export { loft4 };
 export { loft5 };
-export { loft1L };
 export { loft2L };
 export { loft3L };
 export { loft4L };
